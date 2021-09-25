7348eaa1352a79109d267a8b1ba34a070dc15f83
ABDULRAHMAN AL-MYMAN_عبدالرحمن الميمان <66333040+PYTHON01100100@users.noreply.github.com>
2021-09-25 12:51:29 +0300

    Add files via upload

---
+++ عال 212/Fall 2019/Fall 2019/PA/PA1/code/MovieUtils.java
@@ -0,0 +1,18 @@
+public class MovieUtils {
+	// Return the movie with the given id if found, null otherwise.
+	public static Movie findMovieById(List<Movie> movies, int id) {
+		return null;
+	}
+	// Return the list of movies having a given title. If none is found, empty list is returned.
+	public static List<Movie> findMovieByTitle(List<Movie> movies, String title) {
+		return null;
+	}
+	// Return the list of movies of a given genre. If none is found, empty list is returned.
+	public static List<Movie> findMoviesByGenre(List<Movie> movies, String genre) {
+		return null;
+	}
+	// Return the list of movies of given genres (a movie must have all genres to be in the list). If none is found, empty list is returned. Assume genres is not empty.
+	public static List<Movie> findMoviesByGenres(List<Movie> movies, List<String> genres) {
+		return null;
+	}
+}
